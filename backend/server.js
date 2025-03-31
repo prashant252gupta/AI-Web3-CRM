636a9aa75e87a3759e611e36ac5d8b776e789cb0
prashant252gupta <pguptaprashant252@gmail.com>
2025-04-01 01:42:13 +0530

    connected frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+process.on("SIGTERM", () => {
+  console.log("Process terminated! Cleaning up...");
+});
