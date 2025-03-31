c9f509ca5a5dc2aa344f97d1355553928ab0921a
prashant252gupta <pguptaprashant252@gmail.com>
2025-04-01 01:14:41 +0530

    connected frontend to backend

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -3,6 +3,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const fetchData = async (endpoint) => {
   try {
     const response = await fetch(`${API_BASE_URL}/${endpoint}`);
+    if (!response.ok) throw new Error("Failed to fetch data");
     return await response.json();
   } catch (error) {
     console.error("API Error:", error);
